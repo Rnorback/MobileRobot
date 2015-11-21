406cffee43780754f356ccedce46fc0688109827
rnorback <rnorback@gmail.com>
2015-11-21 12:21:31 -0700

    Initial Commit

---
+++ MobileRobot/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MobileRobot
+//
+//  Created by Rob Norback on 11/21/15.
+//  Copyright © 2015 Norback Solutions, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MobileRobot/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MobileRobot
+//
+//  Created by Rob Norback on 11/21/15.
+//  Copyright © 2015 Norback Solutions, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
